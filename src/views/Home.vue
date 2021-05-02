f1354ae19262b5fdf543608fb253f7b809d83023
eminzzz <eminroth@gmail.com>
2021-05-02 05:38:45 +0300

    chore: added 3 links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>Home page</div>
+  <div class="container">
+    <navigation-link :author="true" text="Serious Sam" />
+    <navigation-link text="Bot status" />
+    <navigation-link text="Edit menu" />
+  </div>
 </template>
 
 <script>
+import NavigationLink from "../components/NavigationLink";
+
 export default {
   name: "Home",
+  components: {
+    NavigationLink,
+  },
 };
 </script>
