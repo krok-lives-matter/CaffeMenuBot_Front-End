b33f71ac3ea8d01282800fbaf0fddfadb064f02f
eminzzz <eminroth@gmail.com>
2021-05-06 05:09:26 +0300

    feat: create main layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="main-wrapper">
+      <Navigation />
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from "../components/Navigation";
+
+export default {
+  name: "MainLayout",
+  components: { Navigation },
+};
+</script>
+
+<style scoped lang="scss">
+.main-wrapper {
+  padding-top: 40px;
+  display: flex;
+}
+</style>
