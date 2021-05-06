ae14bcfc8f27e4706835c557489df1073ce57f9b
eminzzz <eminroth@gmail.com>
2021-05-06 05:14:49 +0300

    feat: create user view

---
+++ src/views/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <MainLayout>
+    <h1>User</h1>
+  </MainLayout>
+</template>
+
+<script>
+import MainLayout from "../layouts/MainLayout";
+
+export default {
+  name: "User",
+  components: { MainLayout },
+};
+</script>
+
+<style scoped></style>
