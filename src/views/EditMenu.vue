7e9b0f8c34c14ae6268f4ffdb343c7ad485e41c4
eminzzz <eminroth@gmail.com>
2021-05-06 05:15:48 +0300

    feat: create edit menu view

---
+++ src/views/EditMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <MainLayout>
+    <div class="edit-menu">
+      <h1>Menu</h1>
+      <AddBtn />
+    </div>
+  </MainLayout>
+</template>
+
+<script>
+import MainLayout from "../layouts/MainLayout";
+import AddBtn from "../components/AddBtn";
+
+export default {
+  name: "EditMenu",
+  components: { AddBtn, MainLayout },
+};
+</script>
+
+<style scoped lang="scss">
+.edit-menu {
+  width: 100%;
+}
+</style>
