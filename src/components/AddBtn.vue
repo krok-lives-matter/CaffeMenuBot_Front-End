616ba7b07d29fda8d0560e5a916baa94ec9d62e4
eminzzz <eminroth@gmail.com>
2021-05-06 05:16:38 +0300

    feat: create add button component

---
+++ src/components/AddBtn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="add-btn">
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M20 40V0M0 20H40" stroke="#593C8F" stroke-width="2" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddBtn",
+};
+</script>
+
+<style scoped lang="scss">
+.add-btn {
+  width: 150px;
+  height: 150px;
+  border-radius: 5px;
+  background: #eeeef2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: 0.3s all ease-out;
+  &:hover {
+    background: #e6e6ec;
+  }
+}
+</style>
