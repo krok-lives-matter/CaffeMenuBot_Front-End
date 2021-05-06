3fe7cd2a3ee6dc38eab25e8a7ca66953822b27d0
eminzzz <eminroth@gmail.com>
2021-05-06 05:16:08 +0300

    chore: move styles to scss file

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<style lang="scss">
-* {
-  outline: none;
-}
-body {
-  background: #fff;
-  font-size: 18px;
-  font-weight: 400;
-  font-family: "Segoe UI", sans-serif;
-  color: #111111;
-}
-.container {
-  max-width: 100%;
-  padding: 0 40px;
-}
-</style>
