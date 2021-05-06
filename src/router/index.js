3c41bd4e0a427dc0b05888cd805f87953caa49c6
eminzzz <eminroth@gmail.com>
2021-05-06 05:10:50 +0300

    feat: create lazy routes

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,16 @@ const routes = [
     name: "Home",
     component: () => import("../views/Home.vue"),
   },
+  {
+    path: "/edit",
+    name: "EditMenu",
+    component: () => import("../views/EditMenu.vue"),
+  },
+  {
+    path: "/user",
+    name: "User",
+    component: () => import("../views/User.vue"),
+  },
 ];
 
 const router = new VueRouter({
