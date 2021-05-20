b8e73afeaa26f6645a5ef2c150cd0899690d9dce
eminzzz <eminroth@gmail.com>
2021-05-20 04:11:41 +0300

    feat: changes in main * create axios baseurl * create router auth check

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 import "normalize.css/normalize.css";
 import "./assets/scss/_fonts.scss";
 import "./assets/scss/all.scss";
@@ -10,6 +11,22 @@ Vue.config.productionTip = false;
 
 Vue.filter("truncate", function (text, stop, clamp) {
   return text.slice(0, stop) + (stop < text.length ? clamp || "..." : "");
+});
+
+axios.defaults.baseURL = "https://cmb-api.vova-lantsov.dev/api";
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!store.state.auth.admin) {
+      router.push({
+        path: "/admin",
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 new Vue({
