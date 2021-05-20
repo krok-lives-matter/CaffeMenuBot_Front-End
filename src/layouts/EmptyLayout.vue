228fb0455dbb059cc807c4f3b644bd2c75fa19bd
eminzzz <eminroth@gmail.com>
2021-05-20 04:07:36 +0300

    feat: create empty layout component

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmptyLayout",
+};
+</script>
+
+<style lang="scss" scoped></style>
