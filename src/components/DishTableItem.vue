a52d348c624eb98f878c4acd295f7652c162f9a6
eminzzz <eminroth@gmail.com>
2021-05-22 21:44:14 +0300

    feat: create dish item component * open/close table body * emit some events

---
+++ src/components/DishTableItem.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="dishes__table-item">
+    <div class="dishes__table-head">
+      <div class="dishes__table-big-title">
+        {{ dish.title }}
+      </div>
+      <div class="dishes__table-controls">
+        <div
+          @click="$emit('editDish', dish.id)"
+          class="dishes-control dishes-control-icon"
+        >
+          <img src="@/assets/images/edit.svg" alt="pic" />
+        </div>
+        <div
+          class="dishes-control dishes-control-icon"
+          @click="$emit('deleteDish', dish.id)"
+        >
+          <img src="@/assets/images/remove.svg" alt="pic" />
+        </div>
+        <div
+          :class="{ 'dishes-control--active': showBody }"
+          @click="toggleTableBody"
+          class="dishes-control dishes-control-icon"
+        >
+          <img src="@/assets/images/arrow-t.svg" alt="pic" />
+        </div>
+      </div>
+    </div>
+    <div v-if="showBody" class="dishes__table-body">
+      <div class="dishes__table-text">
+        <div class="dishes__table-title">Description:</div>
+        <div class="dishes__table-description">
+          {{ dish.description }}
+        </div>
+      </div>
+      <div class="dishes__table-text">
+        <div class="dishes__table-title">Serving:</div>
+        <div class="dishes__table-description">
+          {{ dish.serving }}
+        </div>
+      </div>
+      <div class="dishes__table-text">
+        <div class="dishes__table-title">Price:</div>
+        <div class="dishes__table-description">
+          {{ dish.price }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DishTableItem",
+  props: {
+    dish: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      showBody: false,
+    };
+  },
+  methods: {
+    toggleTableBody() {
+      this.showBody = !this.showBody;
+    },
+  },
+};
+</script>
