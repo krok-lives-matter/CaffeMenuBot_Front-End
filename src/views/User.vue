1508535c1286f37e8411ccf74593b1138f4159e1
eminzzz <eminroth@gmail.com>
2021-05-27 18:37:49 +0300

    chore: update user page

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,19 +1,31 @@
 <template>
   <MainLayout>
-    <div>
-      <h1>Hello, Admin</h1>
-      <!--      <p>Your email: {{ $store.state.auth.admin.user.email }}</p>-->
-      <!--      <p>Your id: {{ $store.state.auth.admin.user.id }}</p>-->
+    <div v-if="admin">
+      <h1>Hello, {{ admin.userName }}</h1>
+      <p>Your email: {{ admin.email }}</p>
+      <p>Your id: {{ admin.id }}</p>
+      <p>Today is: {{ dateToday }}</p>
     </div>
   </MainLayout>
 </template>
 
 <script>
 import MainLayout from "../layouts/MainLayout";
+import { mapGetters } from "vuex";
 
 export default {
   name: "User",
   components: { MainLayout },
+  computed: {
+    ...mapGetters({ admin: "auth/getAdmin" }),
+    dateToday() {
+      const now = new Date();
+      return `${now.getDate()}/${now.getUTCMonth() + 1}/${now.getFullYear()}`;
+    },
+  },
+  async mounted() {
+    await this.$store.dispatch("auth/getMe");
+  },
 };
 </script>
 
