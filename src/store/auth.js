327d0243423d7e78a7f1f7a44ce2dac0db2f28f5
eminzzz <eminroth@gmail.com>
2021-05-27 18:43:52 +0300

    chore: feat and fixes * add admin field to state * create admin getter * create SET_ADMIN mutation * set axios defaults headers in SET_JWT mutation * realized getMe action * rename SET_ADMIN to SET_JWT mutation * rename REMOVE_ADMIN to REMOVE_JWT mutation

--- src/store/auth.js
+++ src/store/auth.js
@@ -4,15 +4,25 @@ export default {
   namespaced: true,
   state: {
     jwt: localStorage.getItem("jwt"),
+    admin: null,
+  },
+  getters: {
+    getAdmin(state) {
+      return state.admin;
+    },
   },
   mutations: {
-    SET_ADMIN(state, admin) {
+    SET_JWT(state, admin) {
       state.jwt = admin.token;
+      axios.defaults.headers["Authorization"] = "Bearer " + state.jwt;
       localStorage.setItem("jwt", admin.token);
     },
-    REMOVE_ADMIN(state) {
+    REMOVE_JWT(state) {
       state.jwt = "";
       localStorage.removeItem("jwt");
+    },
+    SET_ADMIN(state, data) {
+      state.admin = data;
     },
   },
   actions: {
@@ -20,11 +30,22 @@ export default {
       try {
         const { data, status } = await axios.post("/auth/login", admin);
         if (status === 200) {
-          commit("SET_ADMIN", data);
+          commit("SET_JWT", data);
           return true;
         }
       } catch (e) {
-        console.error("[Error]: Auth Admin");
+        console.error("[Error]: Auth admin");
+        throw new Error(e.message);
+      }
+    },
+    async getMe({ commit }) {
+      try {
+        const { data, status } = await axios.get("/auth/me");
+        if (status === 200) {
+          commit("SET_ADMIN", data);
+        }
+      } catch (e) {
+        console.error("[Error]: Get me");
         throw new Error(e.message);
       }
     },
