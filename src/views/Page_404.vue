50660c16224e99a14b883fcf4335c9f6f0f38030
eminzzz <eminroth@gmail.com>
2021-05-27 18:37:29 +0300

    feat: create 404 page

---
+++ src/views/Page_404.vue
@@ -0,0 +1,19 @@
+<template>
+  <EmptyLayout>
+    <div class="content">
+      <div><h1>404 Page not found</h1></div>
+      <div>Go to <router-link to="/">main page</router-link></div>
+    </div>
+  </EmptyLayout>
+</template>
+
+<script>
+import EmptyLayout from "../layouts/EmptyLayout";
+
+export default {
+  name: "Page_404",
+  components: { EmptyLayout },
+};
+</script>
+
+<style scoped></style>
